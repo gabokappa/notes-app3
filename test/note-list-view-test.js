f4c5e3082ecc0e210117a9f4e8ad62bf03c69826
Rafaela <rafalucy3@gmail.com>
2019-10-03 17:32:08 +0100

    Spike potential solution to 9;3

--- test/note-list-view-test.js
+++ test/note-list-view-test.js
@@ -4,7 +4,6 @@ function returnsListOfNotes() {
   noteList.createNote("This is my new note - remember to water the plants");
   noteList.createNote("This is the second note - remember to feed the dog and cat");
   noteListView = new NoteListView(noteList);
-  console.log(noteListView.returnsView());
   assert.isTrue(noteListView.returnsView() === "<ul><div> <a href='#notes/4'>This is my new note ...</a> </div><div> <a href='#notes/5'>This is the second n...</a> </div></ul>", nameOfFunc[1]);}
 
 returnsListOfNotes();
